367d91d6b3c5f509798d1a792c7e45c494f67e1b
devcronberg <devcronberg@gmail.com>
2023-01-16 15:19:44 +0100

    Update app.js

--- 005array/app.js
+++ 005array/app.js
@@ -7,11 +7,26 @@ a.push(5);
 console.log(a);
 var b = a.pop();
 console.log("b=" + b);
+
+a.forEach(function(v,i,a){
+  console.log(v);
+});
+
 console.log(a);
 a.sort();
 console.log(a);
 
 // ekstra
+
+a.sort(function(a,b){
+  if(a<b)
+    return -1;
+  if(a>b)
+    return 1;
+  return 0;
+});
+
+
 a.sort((a,b)=>{
   if(a<b)
     return -1;
